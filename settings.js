ba1380b24fc07646f876e9f077dfd83a19f01035
yyyiiiizuuuttttttpsss <yyyiiiizuuuttttttpsss@outlook.com>
2025-02-15 06:27:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=C2pASRLA#N7sQ4fHLrd6Xb5-khKMLyyVD0L1C7r9VBKcRaVtEE3U' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
